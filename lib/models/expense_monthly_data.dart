124d380f17ab49988a13b471a8680b7176806117
gsurjs <155509989+gsurjs@users.noreply.github.com>
2025-10-19 13:33:00 -0400

    adding helper class for monthly expense data

---
+++ lib/models/expense_monthly_data.dart
@@ -0,0 +1,6 @@
+class ExpenseMonthlyData {
+  final String month; // Will be in 'YYYY-MM' format
+  final double totalCost;
+
+  ExpenseMonthlyData({required this.month, required this.totalCost});
+}
