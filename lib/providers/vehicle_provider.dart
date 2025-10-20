d7958dc6c33809b28dbadd8bf3f25a8de7c40502
gsurjs <155509989+gsurjs@users.noreply.github.com>
2025-10-19 23:27:15 -0400

    creating central vehicleprovider class to update screens when data changes

---
+++ lib/providers/vehicle_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../models/vehicle.dart';
+import '../services/database_helper.dart';
+
+class VehicleProvider with ChangeNotifier {
+  List<Vehicle> _vehicles = [];
+  bool _isLoading = true;
+
+  List<Vehicle> get vehicles => _vehicles;
+  bool get isLoading => _isLoading;
+
+  VehicleProvider() {
+    fetchVehicles();
+  }
+
+  Future<void> fetchVehicles() async {
+    _isLoading = true;
+    notifyListeners();
+    _vehicles = await DatabaseHelper.instance.readAllVehicles();
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> addVehicle(Vehicle vehicle) async {
+    await DatabaseHelper.instance.createVehicle(vehicle);
+    await fetchVehicles(); // This will refresh the list and notify all listeners
+  }
+}
